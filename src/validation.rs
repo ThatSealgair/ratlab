2db0a126901f049e1e47314297572b7a0df81b09
q-horton <106427075+q-horton@users.noreply.github.com>
2023-08-26 00:27:49 +1000

    Added basic data to validation.rs

--- src/validation.rs
+++ src/validation.rs
@@ -1,1 +1,12 @@
-/// Token validation for ratlab
+/* Token validation for ratlab
+ */ 
+mod header;
+
+// Library imports
+use crates::header::*;
+
+/* Main function for token validation.
+ */
+fn ratlab_validation(tokens: Vec<TokenType>) -> Vec<TokenType> {
+    
+}
