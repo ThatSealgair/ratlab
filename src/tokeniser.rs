62d17fe0cfb1468c8d76d194677dd07fc9bbd31a
Jay Hunter <hunter.jay.k@gmail.com>
2023-08-25 23:21:24 +1000

    Token parsing continued innovation

--- src/tokeniser.rs
+++ src/tokeniser.rs
@@ -1,47 +1,67 @@
 /// tokeniser for ratlab
-/// \
+
+mod header;
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[allow(missing_docs)]
 #[serde(tag = "type")]
 pub enum TokenType {
   // Logical or Boolean types
-  Bool(usize),
+  Bool,
   Conditional(String),
+  Arithmetic(String),
   // Primitive Types
-  Char(char),
-  Single(f32),
-  Double(f64),
-  Iint8(usize),
-  Uint8(usize),
-  Iint16(usize),
-  Uint16(usize),
-  Iint32(usize),
-  Uint32(usize),
-  Iint64(usize),
-  Uint64(usize),
+  Char,
+  Single,
+  Double,
+  Int8,
+  Uint8,
+  Int16,
+  Uint16,
+  Int32,
+  Uint32,
+  Int64,
+  Uint64,
   Identifier(String),
   Equals,
   SemiColon,
   Comma,
-  Plus,
-  Minus,
   End,
   Indentation(bool),
 }
 
-fn tokenize_string(data: &str) -> Result<(Vec<TokenType>, usize)> {
+
+fn tokenize_string(data: String) -> TokenType {
+  match data {
+    types.CHAR => return TokenType::Char,
+    types.BOOL => return TokenType::Bool,
+    types.INT_8 => return TokenType::Int8,
+    types.UINT_8 => return TokenType::Uint8,
+    types.INT_16 => return TokenType::Int16,
+    types.UINT_16 => return TokenType::Uint16,
+    types.INT_32 => return TokenType::Int32,
+    types.UINT_32 => return TokenType::Uint32,
+    types.INT_64 => return TokenType::Int64,
+    types.UINT_64 => return TokenType::Uint64,
+    types.FLOAT => return TokenType::Single,
+    types.DOUBLE => return TokenType::Double,
+    _ => return TokenType::TokenType::Identifier(data),
+  } 
+}
+
+
+
+fn tokenize(data: &str) -> Result<(Vec<TokenType>) {
   let mut tokens = Vec::new();
   let mut line = Vec::new()
-  let mut index = 0;
 
   for ch in data.chars() {
     let mut wasWhitespace = false;
     let mut whitespaceCount = 0;
     
     if (ch.is_whitespace()) {
-      if (!wasWhitespace) {
-        tokens.push(line);
+      if (!wasWhitespace && !tokens.is_empty()) {
+        tokens.push(line.into_iter().collect()::<String>());
         line.clear();
       }
 
@@ -63,6 +83,6 @@ fn tokenize_string(data: &str) -> Result<(Vec<TokenType>, usize)> {
       line.push(ch);
     }
   }
-
-  index += 1;
+  
+  return tokens;
 }
