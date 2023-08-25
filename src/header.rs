1f8ef397bef4fd035b8d259dae5bb24364ee641b
Jay Hunter <hunter.jay.k@gmail.com>
2023-08-26 08:03:26 +1000

    enum good

--- src/header.rs
+++ src/header.rs
@@ -22,18 +22,28 @@ pub enum TokenType {
     Int64,
     Uint64,
     // Syntax
-    Tab,
-    Space,
-    SemiColon,
-    Period,
-    LeftBrace,
-    RightBrace,
-    LeftBracket,
-    RightBracket,
-    Colon,
-    Comma,
-    Equals,
+    Syntax(Syntax),
+
     Identifier(String),
+}
+
+
+impl TokenType {
+    pub fn to_char(&self) -> &str {
+        match self {
+            Syntax::Tab => '\t',
+            Syntax::Space => ' ',
+            Syntax::SemiColon => ';',
+            Syntax::Colon => ':',
+            Syntax::Peroid => '.',
+            Syntax::Comma => ',',
+            Syntax::Equals => '=',
+            Syntax::LeftBrace => '(',
+            Syntax::RightBrace => ')',
+            Syntax::LeftBracket => '{',
+            Syntax::RightBracket => '}',
+        }
+    }
 }
 
 pub mod glitter {
@@ -58,54 +68,132 @@ pub mod types {
     pub const STRING: &str = "&ret_hc";
 }
 
-pub mod conditional {
-    pub const AND: &str = "&";
-    pub const NOT: &str = "~";
-    pub const OR: &str = "|";
-    pub const XOR: &str = "xor";
-    pub const TRUE: &str = "true";
-    pub const FALSE: &str = "false";
-    pub const LESS: &str = "<";
-    pub const GREATER: &str = ">";
-    pub const EQUALS: &str = "==";
-    pub const LESS_EQ: &str = "<=";
-    pub const GREATER_EQ: &str = ">=";
-    pub const NOT_EQ: &str = "~=";
+pub enum Conditional {
+    And = "&",
+    Not = "~",
+    Or = "|",
+    Xor = "xor",
+    True = "true",
+    False = "false",
+    Less = "<",
+    Greater = ">",
+    Equals = Syntax.Equals + Syntax.Equals,
+    LessEq = Less + Syntax.Equals,
+    GreaterEq = Greater + Syntax.Equals,
+    NotEq = Not + Syntax.Equals,
 }
 
-pub mod math_operator {
-    pub const PLUS: &str = "+";
-    pub const MINUS: &str = "-";
-    pub const TIMES: &str = "*";
-    pub const DIVIDE: &str = "/";
-    pub const POWER: &str = "^";
-    // To be constructed later from period
-    // pub const ELEMENT_WISE: &str = ".";
+impl Syntax {
+    pub fn to_char(&self) -> char {
+        match self {
+            Syntax::Tab => '\t',
+            Syntax::Space => ' ',
+            Syntax::SemiColon => ';',
+            Syntax::Colon => ':',
+            Syntax::Peroid => '.',
+            Syntax::Comma => ',',
+            Syntax::Equals => '=',
+            Syntax::LeftBrace => '(',
+            Syntax::RightBrace => ')',
+            Syntax::LeftBracket => '{',
+            Syntax::RightBracket => '}',
+        }
+    }
 }
 
-pub mod statements {
-    pub const WHILE: &str = "while";
-    pub const FOR: &str = "for";
-    pub const IF: &str = "if";
-    pub const ELIF: &str = "elseif";
-    pub const ELSE: &str = "else";
-    pub const SWITCH: &str = "switch";
-    pub const CASE: &str = "case";
-    pub const DEFAULT: &str = "otherwise";
-    pub const FN: &str = "function";
-    pub const CLOSE: &str = "end";
+impl Syntax {
+    pub fn to_char(&self) -> char {
+        match self {
+            Syntax::Tab => '\t',
+            Syntax::Space => ' ',
+            Syntax::SemiColon => ';',
+            Syntax::Colon => ':',
+            Syntax::Peroid => '.',
+            Syntax::Comma => ',',
+            Syntax::Equals => '=',
+            Syntax::LeftBrace => '(',
+            Syntax::RightBrace => ')',
+            Syntax::LeftBracket => '{',
+            Syntax::RightBracket => '}',
+        }
+    }
 }
 
-pub mod syntax {
-    pub const TAB: char = '\t';
-    pub const SPACE: char = ' ';
-    pub const SEMI_COLON: char = ';';
-    pub const PERIOD: char = '.';
-    pub const LEFT_BRACE: char = '(';
-    pub const RIGHT_BRACE: char = ')';
-    pub const LEFT_BRACKET: char = '{';
-    pub const RIGHT_BRACKET: char = '}';
-    pub const COLON: char = ':';
-    pub const COMMA: char = ',';
-    pub const EQUALS: char = '=';
+pub enum ArithmeticOperator {
+    Plus = "+",
+    Minus = "-",
+    Times = "/",
+    Power = "^",
+}
+
+impl ArithmeticOperator {
+    pub fn to_char(&self) -> char {
+        match self {
+            ArithmeticOperator::Plus => '+',
+            ArithmeticOperator::Minus => '-',
+            ArithmeticOperator::Times => '/',
+            ArithmeticOperator::Power => '^',
+        }
+    }
+}
+
+
+pub enum Statements {
+    While,
+    For,
+    If,
+    ElseIf,
+    Switch,
+    Case,
+    Default,
+    Function,
+    Close,
+}
+
+impl Statements {
+    pub fn to_string(&self) -> &str {
+        match self {
+            Statements::While => "while",
+            Statements::For => "for",
+            Statements::If => "if",
+            Statements::ElseIf => "elseif",
+            Statements::Switch => "switch",
+            Statements::Case => "case",
+            Statements::Default => "otherwise",
+            Statements::Function => "function",
+            Statements::Close => "end",
+        }
+    }
+}
+
+pub enum Syntax {
+    Tab,
+    Space,
+    SemiColon,
+    Colon,
+    Peroid,
+    Comma,
+    Equals,
+    LeftBrace,
+    RightBrace,
+    LeftBracket,
+    RightBracket,
+}
+
+impl Syntax {
+    pub fn to_char(&self) -> char {
+        match self {
+            Syntax::Tab => '\t',
+            Syntax::Space => ' ',
+            Syntax::SemiColon => ';',
+            Syntax::Colon => ':',
+            Syntax::Peroid => '.',
+            Syntax::Comma => ',',
+            Syntax::Equals => '=',
+            Syntax::LeftBrace => '(',
+            Syntax::RightBrace => ')',
+            Syntax::LeftBracket => '{',
+            Syntax::RightBracket => '}',
+        }
+    }
 }
