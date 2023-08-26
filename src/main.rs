91faa50fec9b891baf4da6dcb6682549b738854c
q-horton <106427075+q-horton@users.noreply.github.com>
2023-08-26 20:32:00 +1000

    Updated main.rs to take user input on file.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
+use std::env;
+
 use ratlib::input::*;
 use ratlib::header::*;
 
 fn main() {
-    let lines: Vec<String> = ratlab_input("file");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Incorrect usage, please try 'ratlab [file]'.");
+    }
+    let lines: Vec<String> = ratlab_input(args[1].as_str());
     
-    println!("Type name for characters is {}.", types::CHAR);
+    println!("Contents of {} are:", args[1].as_str());
     println!("----------");
-
+    
+    let mut index = 0;
     for line in lines.iter() {
-        for string in line.lines() {
-            print!("{}", string);
-        }
-        //println!("");
+        println!("{index} | {line}");
+        index += 1;
     }
 }
