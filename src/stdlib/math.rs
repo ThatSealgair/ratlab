56b743d324b1437bcc6d3c40c563c10d23da9e9a
q-horton <106427075+q-horton@users.noreply.github.com>
2023-08-26 18:35:08 +1000

    Added a variety of additional mathematical functions to the standard ratlab library.

--- src/stdlib/math.rs
+++ src/stdlib/math.rs
@@ -15,8 +15,8 @@ fn int_from_generic<T: cast::ToPrimitive>(n: T) -> i64 {
 }
 
 /* Converts a generic to a float or throws an error. */
-fn float_from_generic<T: cast::ToPrimitive>(n: T) -> f64 {
-    match n.to_f64() {
+fn float_from_generic<T: cast::ToPrimitive>(x: T) -> f64 {
+    match x.to_f64() {
         None => panic!("Factorial is not a floating point number."),
         Some(val) => val,
     }
@@ -30,9 +30,9 @@ fn float_from_generic<T: cast::ToPrimitive>(n: T) -> f64 {
  *
  * Returns: A float representing the absolute value of n.
  */
-pub fn absolute<T: cast::ToPrimitive>(n: T) -> f64 {
-    let n_float: f64 = float_from_generic(n);
-    if n_float >= 0f64 {n_float} else {-n_float}
+pub fn absolute<T: cast::ToPrimitive>(x: T) -> f64 {
+    let x_float: f64 = float_from_generic(x);
+    if x_float >= 0f64 {x_float} else {-x_float}
 }
 
 /* average()
@@ -52,6 +52,37 @@ pub fn average<T: cast::ToPrimitive>(values: Vec<T>) -> f64 {
         count += 1;
     }
     sum / (count as f64)
+}
+
+/* ceil()
+ * -----
+ * Finds the ceiling of the provided value (rounds up to the next int)
+ * 
+ * x: The value to round up.
+ *
+ * Returns: The integer equal to x rounded up.
+ */
+pub fn ceil<T: cast::ToPrimitive>(x: T) -> i64 {
+    let x_int: i64 = int_from_generic(x);
+    let fix_x: i64 = fix(x);
+    if x_int < 0 || x_int == fix_x {
+        return fix_x;
+    } else {
+        return fix_x + 1;
+    }
+}
+
+/* cosine()
+ * -----
+ * Calculates the value of cos(x).
+ *
+ * x: The value to find the cosine of.
+ *
+ * Returns: The value cos(x).
+ */
+pub fn cosine<T: cast::ToPrimitive>(x: T) -> f64 {
+    let x_float: f64 = float_from_generic(x);
+    x_float.cos()
 }
 
 /* exponential()
@@ -77,6 +108,14 @@ pub fn exponential<P: cast::ToPrimitive + PrimInt, B: cast::ToPrimitive>(power: 
     result
 }
 
+/* factorial()
+ * -----
+ * Calculates the factorial of an unsigned integer.
+ * 
+ * n: The number to calculate the factorial of.
+ *
+ * Returns: An unsigned integer equal to the factorial of n.
+ */
 pub fn factorial<T: PrimInt + Unsigned + cast::ToPrimitive>(n: T) -> u64 {
     let n_int: i64 = int_from_generic(n);
     if n_int < 0 {
@@ -87,4 +126,189 @@ pub fn factorial<T: PrimInt + Unsigned + cast::ToPrimitive>(n: T) -> u64 {
         fact *= i;
     }
     fact
+}
+
+/* fix()
+ * -----
+ * Finds the fix of the value provided (strips away any non-integer component).
+ * 
+ * x: The number to strip the decimal from.
+ *
+ * Returns: The integer component of x.
+ */
+pub fn fix<T: cast::ToPrimitive>(x: T) -> i64 {
+    let x_float: f64 = float_from_generic(x);
+    int_from_generic(x_float % 1f64)
+}
+
+/* floor()
+ * -----
+ * Calculates the floor of the value provided (rounds down to the next int).
+ * 
+ * x: The number to round down.
+ *
+ * Returns: The resultant floor.
+ */
+pub fn floor<T: cast::ToPrimitive>(x: T) -> i64 {
+    let x_int: i64 = int_from_generic(x);
+    let fix_x: i64 = fix(x);
+    if x_int > 0 || x_int == fix_x {
+        return fix_x;
+    } else {
+        return fix_x - 1;
+    }
+}
+
+/* log_e()
+ * -----
+ * Takes the natural logarithm.
+ *
+ * x: The value to log.
+ *
+ * Returns: The natural logarithm of x, ln(x).
+ */
+pub fn log_e<T: cast::ToPrimitive>(x: T) -> f64 {
+    let x_float: f64 = float_from_generic(x);
+    x_float.ln()
+}
+
+/* log_2()
+ * -----
+ * Takes the base 2 logarithm.
+ *
+ * x: The value to log.
+ *
+ * Returns: The base two logarithm of x, log2(x).
+ */
+pub fn log_2<T: cast::ToPrimitive>(x: T) -> f64 {
+    let x_float: f64 = float_from_generic(x);
+    x_float.log2()
+}
+
+/* log_10()
+ * -----
+ * Takes the base 10 logarithm.
+ *
+ * x: The value to log.
+ *
+ * Returns: The base 10 logarithm of x, ln(x).
+ */
+pub fn log_10<T: cast::ToPrimitive>(x: T) -> f64 {
+    let x_float: f64 = float_from_generic(x);
+    x_float.log10()
+}
+
+/* logarithm()
+ * -----
+ * The general logarithm.
+ *
+ * x: The value to log.
+ * b: The base of the logarithm.
+ *
+ * Returns: The base n logarithm of x, log_n(x).
+ */
+pub fn logarithm<T: cast::ToPrimitive>(x: T, n: T) -> f64 {
+    let x_float: f64 = float_from_generic(x);
+    let n_float: f64 = float_from_generic(n);
+    x_float.log(n_float)
+}
+
+/* modulus()
+ * -----
+ * Calculates the value of x (mod div).
+ *
+ * x: The value to take the modulus of.
+ * div: The divisor to take the remainder of.
+ *
+ * Returns: The value of x (mod div).
+ */
+pub fn modulus<T: cast::ToPrimitive>(x: T, div: T) -> f64 {
+    let x_float = float_from_generic(x);
+    let div_float = float_from_generic(div);
+    let rem: f64 = x_float % div_float;
+    if rem < 0f64 {
+        return rem + div_float;
+    } else {
+        return rem;
+    }
+}
+
+/* round()
+ * -----
+ * Rounds the provided value to the nearest integer.
+ *
+ * x: The value to round.
+ *
+ * Returns: The closest integer to x.
+ */
+pub fn round<T: cast::ToPrimitive>(x: T) -> i64 {
+    let x_float = float_from_generic(x);
+    if x_float - (x_float % 1f64) < 0.5 {
+        return floor(x);
+    } else {
+        return ceil(x);
+    }
+}
+
+/* sign()
+ * -----
+ * Calculates the sign of the given number.
+ *
+ * n: The number to find the sign of.
+ *
+ * Returns: 1 if positive, -1 if negative, 0 if zero
+ */
+pub fn sign<T: cast::ToPrimitive>(n: T) -> i8 {
+    let n_int: f64 = float_from_generic(n);
+    if n_int == 0f64 {
+        return 0;
+    } else if n_int / absolute(n_int) > 0f64 {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+/* sine()
+ * -----
+ * Calculates the value of sin(x).
+ *
+ * x: The value to find the sine of.
+ *
+ * Returns: The value sin(x).
+ */
+pub fn sine<T: cast::ToPrimitive>(x: T) -> f64 {
+    let x_float: f64 = float_from_generic(x);
+    x_float.sin()
+}
+
+/* square_root()
+ * -----
+ * Calculates the square root of the given value.
+ * 
+ * x: The value to find the root of.
+ *
+ * Returns: The square root of x.
+ */
+pub fn numerical_root<T: cast::ToPrimitive>(x: T) -> f64 {
+    let float_x: f64 = float_from_generic(x);
+    float_x.sqrt()
+}
+
+/* tangent()
+ * -----
+ * Calculates the value of tan(x).
+ *
+ * x: The value to find the tangent of.
+ *
+ * Returns: The value tan(x).
+ */
+pub fn tangent<T: cast::ToPrimitive>(x: T) -> f64 {
+    let x_float: f64 = float_from_generic(x);
+    if (x_float - consts::PI/2f64) % (2f64 * consts::PI) == 0f64 {
+        return f64::INFINITY;
+    } else if (x_float - consts::PI/2f64) % (consts::PI) == 0f64 {
+        return f64::NEG_INFINITY;
+    }
+    x_float.tan()
 }
