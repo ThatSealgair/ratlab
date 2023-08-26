6cc50389fb070381a0e02f1efdce7cf325dd5935
q-horton <106427075+q-horton@users.noreply.github.com>
2023-08-27 00:40:14 +1000

    Started conversion methods.

--- src/conversion.rs
+++ src/conversion.rs
@@ -3,10 +3,26 @@
 
 use crate::header;
 
+enum TokenClump {
+    Single(TokenType),
+    Clump(Vec<TokenClump>),
+}
+
 /* Takes in the validated tokens and outputs the converted lines.
  */
 pub fn ratlab_conversion(tokens: Vec<TokenType>) -> Vec<String> {
 
 }
 
-
+/* Iterates through a line, breaking it into clumps
+ */
+fn token_clump(tokens: &Vec<TokenType>, index: usize) -> (Vec<TokenClump>, usize) {
+    let clumps: Vec<TokenClump> = Vec::new();
+    for i in 0..tokens.len() {
+        match tokens[i] => {
+            TokenType::Syntax(Syntax::LeftBracket) => token_clump(tokens, index + 1);
+            TokenType::Syntax(Syntax::RightBracket) => return (clumps, index + 1);
+            (_) => clumps.push(tokens[i]);
+        };
+    }
+}
