3f62be9e58a299443fc3f67872b719a3355c1803
q-horton <106427075+q-horton@users.noreply.github.com>
2023-08-27 10:02:24 +1000

    Got pattern matching started on token to rust string process.

--- src/conversion.rs
+++ src/conversion.rs
@@ -10,22 +10,59 @@ enum TokenClump {
 
 /* Takes in the validated tokens and outputs the converted lines.
  */
-pub fn ratlab_conversion(tokens: Vec<TokenType>) -> Vec<String> {
-    Vec::new()
+pub fn ratlab_conversion(tokens: Vec<Vec<TokenType>>) -> Vec<String> {
+    let lines: Vec<String> = Vec::new();
+    for line in tokens {
+        let (clump, _) = token_clump(&line, 0usize);
+        
+    }
+    lines
 }
 
 /* Iterates through a line, breaking it into clumps
  */
-fn token_clump(tokens: &Vec<TokenType>, index: usize) -> (Vec<TokenClump>, usize) {
+fn token_clump(tokens: &Vec<TokenType>, index: usize) -> (TokenClump, usize) {
     let clumps: Vec<TokenClump> = Vec::new();
-    let index: u64 = 0;
-    for token in tokens.iter() {
-        match *token => {
-            TokenType::Syntax(Syntax::LeftBracket) => token_clump(tokens, index + 1),
-            TokenType::Syntax(Syntax::RightBracket) => return (clumps, index + 1),
-            (_) => clumps.push(tokens[i]),
+    let mut i: usize = index;
+    for token in tokens[index..].iter() {
+        let (clump_vec, _) = match *token {
+            TokenType::Syntax(Syntax::LeftBracket) => token_clump(tokens, i + 1usize),
+            TokenType::Syntax(Syntax::RightBracket) => return (TokenClump::Clump(clumps), i + 1usize),
+            var => (TokenClump::Single(var), index),
         };
-        index += 1;
+        i += 1;
     }
-    (Vec::new(), 0)
+    (TokenClump::Clump(clumps), i)
 }
+
+/* Converts token clumps to rust strings.
+ */
+fn clump_to_string(clump: TokenClump) -> String {
+    let mut needs_wrap = false;
+    match clump {
+        TokenClump::Single(var) => {
+            let mut string = String::new();
+            if needs_wrap {string.push(syntax::LEFT_BRACKET)};
+            string.push_str(match var {
+                TokenType::PrimitiveType(type) => "".to_string(),
+                TokenType::Conditional(type) => "".to_string(),
+                TokenType::Syntax(type) => "".to_string(),
+                TokenType::Identifier(type) => "".to_string(),
+                TokenType::Statements(type) => "".to_string(),
+                TokenType::ArithmeticOperator(type) => "".to_string(),
+            }.as_str());
+            if needs_wrap {string.push(syntax::RIGHT_BRACKET)};
+            string
+        },
+        TokenClump::Clump(vars) => {
+            let mut string = String::new();
+            string.push(syntax::LEFT_BRACKET);
+            for c in vars {
+                string.push_str(clump_to_string(c).as_str());
+            }
+            string.push(syntax::RIGHT_BRACKET);
+            string
+        }
+    }
+}
+
